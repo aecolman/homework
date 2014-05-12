9a027c4caf9d70ac5b0dfab628dae663b960aed6
vagrant <vagrant@kellogg-dev-box>
2014-05-12 17:49:54 +0000

    Completed HW

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,11 @@
 class ReviewsController < ApplicationController
 
 def update
-
   Book.read.each do |each_book|
     rating_average = Review.where("book_id" => each_book["id"]).average("rating")
-
     each_book.update("avg_rating" => rating_average)
-
-    end
-
+  end
   redirect_to "/"
-
 end
 
 end
